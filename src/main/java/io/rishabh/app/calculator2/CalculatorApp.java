5c2e773f7d1b9cca5a77d47416017b44550e8461
rishasi <to.rishasi@gmail.com>
2021-05-27 00:32:24 +0530

    Added Add logic

--- src/main/java/io/rishabh/app/calculator2/CalculatorApp.java
+++ src/main/java/io/rishabh/app/calculator2/CalculatorApp.java
@@ -2,6 +2,21 @@ package io.rishabh.app.calculator2;
 
 public class CalculatorApp {
     public String execute(String command) {
+
+        int sum = 0;
+        String[] input = command.split(" ");
+        String operation = input[0];
+        String[] values = input[1].split(",");
+
+        if(operation.equals("add")){
+            for(String value : values){
+                sum = sum + Integer.parseInt(value);
+            }
+            return Integer.toString(sum);
+        }
+
+
         return "Invalid command";
+
     }
 }
