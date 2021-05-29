14774bd00dbe2610f6a02764eb334430e040bfd3
rishasi <to.rishasi@gmail.com>
2021-05-29 22:51:54 +0530

    Implemented ArrayList instead of an Array for Command[]

--- src/main/java/io/rishabh/app/calculator2/CalculatorApp.java
+++ src/main/java/io/rishabh/app/calculator2/CalculatorApp.java
@@ -1,20 +1,12 @@
 package io.rishabh.app.calculator2;
 
+import java.util.ArrayList;
+
 public class CalculatorApp {
-    private int numOfCommands = 0;
-    private Command[] commands = new Command[10];
+    private ArrayList<Command> commands = new ArrayList<Command>();
 
     public void addCommand(Command command){
-        if(numOfCommands == commands.length){
-            int oldSize = commands.length;
-            Command [] moreCommands = new Command[commands.length + 10];
-            for(int i = 0; i < oldSize; i++){
-                moreCommands[i] = commands[i];
-            }
-            commands = moreCommands;
-        }
-        commands[numOfCommands] = command;
-        numOfCommands++;
+        commands.add(command);
     }
 
     public String execute(String command) {
@@ -23,9 +15,9 @@ public class CalculatorApp {
         String operation = input[0];
         String[] values = input[1].split(",");
 
-        for(int i = 0; i < numOfCommands; i++){
-            if(commands[i].appliesTo(operation)){
-                return commands[i].execute(values);
+        for(Command myCommand : commands){
+            if(myCommand.appliesTo(operation)){
+                return myCommand.execute(values);
             }
         }
         return "Invalid command";
